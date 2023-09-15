f1a7fe2c50f62a935e261dd3bc17ae2f944aba0a
Umair513 <soomroumair513@gmail.com>
2023-09-15 12:18:34 +0500

    done with authorization

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const morgan = require("morgan")
 const connectDB = require("./config/db")
-require("dotenv").config({path: "./config/config.env"})
-
+require("dotenv").config({ path: "./config/config.env" })
+const auth = require("./middlewares/auth")
 
 const app = express()
 
@@ -12,7 +12,9 @@ app.use(express.json())
 app.use(morgan("tiny"))
 
 // routes
-
+app.get("/protected", auth, (req, res) => {
+    return res.status(200).json({...req.user._doc})
+})
 app.use("/api", require("./routes/auth"))
 
 // server configuration
